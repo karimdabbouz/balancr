68b5b767b11b2b5525fa1a9a29150d623d468650
karimdabbouz <hey@karim.ooo>
2023-09-20 17:27:06 +0200

    remove unnecessary code

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .extensions import db, scheduler, cache, r
+from .extensions import db
 
 
 
@@ -8,14 +8,8 @@ def init_app():
     app.config.from_object('config.DevConfig')
 
     db.init_app(app)
-    # scheduler.init_app(app)
-    # cache.init_app(app)
-    # r.init_app(app)
 
     with app.app_context():
         from . import routes
-        from . import tasks
-
-        # scheduler.start()
 
         return app
--- application/routes.py
+++ application/routes.py
@@ -1,26 +1,7 @@
 from flask import current_app as app
 from flask import render_template, redirect, url_for, jsonify, request, abort
-from .models import db, table_list, zeitArticles
-from .extensions import scheduler, cache
-from .utils import compute_lda_topics
 
 
 @app.route('/')
 def home():
     return 'balancr-api'
-
-
-@app.route('/api/lda-topics')
-def lda_topics():
-    '''
-    Allows to explicitly recompute LDA topics.
-    '''
-    lda_topics = compute_lda_topics()
-    return jsonify(lda_topics)
-
-
-@app.route('/test-db')
-def test_db():
-    print(type(zeitArticles))
-    # entry = db.session.query(zeitArticles).first()
-    return "yeah"
