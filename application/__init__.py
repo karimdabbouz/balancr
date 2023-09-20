59d9afd1a2b621db25e637185121d07c96adcdff
karimdabbouz <hey@karim.ooo>
2023-09-20 17:20:49 +0200

    first commit

--- application/__init__.py
+++ application/__init__.py
@@ -3,25 +3,19 @@ from .extensions import db, scheduler, cache, r
 
 
 
-
 def init_app():
-    '''
-    Initialize the core application
-    '''
     app = Flask(__name__)
     app.config.from_object('config.DevConfig')
 
     db.init_app(app)
-    scheduler.init_app(app)
-    cache.init_app(app)
-    r.init_app(app)
+    # scheduler.init_app(app)
+    # cache.init_app(app)
+    # r.init_app(app)
 
-    
     with app.app_context():
         from . import routes
         from . import tasks
 
-        # db.create_all()
-        scheduler.start()
+        # scheduler.start()
 
         return app
