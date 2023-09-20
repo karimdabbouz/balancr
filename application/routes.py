10584bb050d60ec9dcfb025df32b08c7a119f585
karimdabbouz <hey@karim.ooo>
2023-09-20 19:52:43 +0200

    add templates with form

--- application/routes.py
+++ application/routes.py
@@ -4,4 +4,11 @@ from flask import render_template, redirect, url_for, jsonify, request, abort
 
 @app.route('/')
 def home():
-    return 'balancr-api'
+    return render_template('index.html')
+
+
+@app.route('/topics', methods=['POST'])
+def compute_topics():
+    print(request.form['startDate'])
+    print(request.form['endDate'])
+    return 'nice'
